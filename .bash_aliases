53c9eca69304c79a943f21bebc06a3816c11f032
Andrew Goessling <agoessling@gmail.com>
2015-11-07 22:43:02 -0800

    Initial commit.

---
+++ .bash_aliases
@@ -0,0 +1,8 @@
+alias cd..='cd ..'
+alias www='google-chrome'
+
+# screen
+alias sc='screen'
+alias sl='screen -ls'
+alias sr='screen -RR'
+alias sx='screen -xx'
---
+++ setup_linux.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Install basic programs.
+sudo apt-get update
+
+sudo apt-get -y install vim
+sudo apt-get -y install git
+sudo apt-get -y install screen
+
+if ! dpkg -s google-chrome-stable | grep -q 'Status: install ok installed'; then
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo dpkg -i google-chrome-stable_current_amd64.deb
+  sudo apt-get -y -f install
+  rm google-chrome-stable_current_amd64.deb
+fi
+
+# Swap Caps-Lock with Escape.
+if ! grep -q 'setxkbmap -option caps:swapescape' ~/.profile; then
+  echo "" >> ~/.profile
+  echo "# Swap Caps-Lock with Escape." >> ~/.profile
+  echo "setxkbmap -option caps:swapescape" >> ~/.profile
+fi
+
+# Configure terminal and Git.
+cp -i .bash_aliases ~/
+cp -i .gitconfig ~/
+cp -i .screenrc ~/
+cp -i .vimrc ~/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+if ! grep -q 'Custom bashrc additions' ~/.bashrc; then
+  cat .bashrc_additions >> ~/.bashrc
+fi
+
+# Install solarized color-scheme.
+sudo apt-get install dconf-cli dconf-editor
+git clone https://github.com/oz123/solarized-mate-terminal.git
+cd solarized-mate-terminal
+read -n1 -r -p 'Create "Solarized" terminal profile.  Press any key to continue.'
+bash solarized-mate.sh dark Solarized
+cd ..
+rm -rf solarized-mate-terminal
