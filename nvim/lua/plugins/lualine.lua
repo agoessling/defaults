9efb6d755187326989b9917dcdc19d551335cc49
Andrew Goessling <agoessling@gmail.com>
2023-01-09 09:55:35 -0800

    Tweak lualine filename display.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,7 +4,16 @@ M.config = function()
   require('lualine').setup {
     options = {
       theme = 'gruvbox-material'
-    }
+    },
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          file_status = true,
+          path = 1,
+        },
+      },
+    },
   }
 end
 
