7e344f7677aab42f6784cb5595377757a65190c8
Andrew Goessling <agoessling@gmail.com>
2023-06-01 22:31:46 -0700

    Moved nvim config to separate repo.

--- nvim/lua/plugins/hop.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-M.config = function()
-  local hop = require('hop')
-  hop.setup { keys = 'etovxqpdygfblzhckisuran' }
-  vim.keymap.set({ 'n', 'v', 'o' }, 'f', hop.hint_char2)
-end
-
-return M
--- setup_linux.sh
+++ setup_linux.sh
@@ -5,15 +5,14 @@ set -euo pipefail
 
 source "$(dirname "$0")/terminal_config.sh"
 
-# Install APT packages.
+# install apt packages.
 sudo apt-get update
 sudo apt-get -y install \
     git \
     pip \
     tmux \
     dconf-cli \
-    uuid-runtime \
-    clangd
+    uuid-runtime
 
 # Install tmux configuration.
 cp tmux/.tmux.conf ~/
@@ -31,9 +30,6 @@ if ! dpkg -s google-chrome-stable | grep -q 'Status: install ok installed'; then
   rm google-chrome-stable_current_amd64.deb
 fi
 
-# Install pyright for NeoVim LSP.
-pip install pyright
-
 # Swap Caps-Lock with Escape.
 if ! grep -q 'setxkbmap -option caps:escape' ~/.profile; then
   echo "" >> ~/.profile
@@ -43,21 +39,23 @@ fi
 
 # Install recent NeoVim
 if ! command -v nvim &> /dev/null; then
-  wget -q --show-progress -O /tmp/nvim.deb https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-  sudo apt-get install /tmp/nvim.deb
-  rm -f /tmp/nvim.deb
+  sudo wget -q --show-progress -O /usr/local/bin/nvim https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+  sudo chmod +x /usr/local/bin/nvim
 fi
 
-# Install NeoVim configuration
-mkdir -p ~/.config/nvim
-cp -r nvim/* ~/.config/nvim
+# Install NVIM configuration.
+if [ ! -d ~/.config/nvim ]; then
+  git clone git@github.com:agoessling/nvim_config.git ~/.config/nvim
+else
+  git pull origin master
+fi
 
 # Download patched fonts.
 mkdir -p ~/.local/share/fonts
-wget -nc -q --show-progress -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
-wget -nc -q --show-progress -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete.ttf
-wget -nc -q --show-progress -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Italic/complete/Hack%20Italic%20Nerd%20Font%20Complete.ttf
-wget -nc -q --show-progress -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/BoldItalic/complete/Hack%20Bold%20Italic%20Nerd%20Font%20Complete.ttf
+wget -nc -q --show-progress -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf
+wget -nc -q --show-progress -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Bold/HackNerdFont-Bold.ttf
+wget -nc -q --show-progress -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Italic/HackNerdFont-Italic.ttf
+wget -nc -q --show-progress -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/BoldItalic/HackNerdFont-BoldItalic.ttf
 
 # Setup terminal colorscheme
 setup_gruvbox_colors
